6ccac4aa9730a1ea33ba1fa3624a62f93419cf65
Linden Melvin <linden.melvin@gmail.com>
2013-10-09 18:23:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onesemble::Application.config.secret_token = 'b58b101928f09350ec475bf084298007e249b32578e90ff6e8e1fd25aa07da16f077eddd9556e85440ae8c54647b55915d8b9f934994d38e1e9c0a2817fc9abd'
