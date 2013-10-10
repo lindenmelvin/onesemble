0ba868ee74a79099b48fcce87b788d1eb8e51ebd
Linden Melvin <linden.melvin@gmail.com>
2013-10-09 22:12:27 -0700

    removing omniauth from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
 end
