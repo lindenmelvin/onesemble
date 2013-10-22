ee969e71f3342c9389cdf5e76013dc33a18cea76
Linden Melvin <linden.melvin@gmail.com>
2013-10-22 08:03:11 -0700

    setting s3 in app intializers

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,6 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
 GENRES = [
   'Pop',
   'New Music',
